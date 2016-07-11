6d8eccf8f848c56ef4535d960c1f5132fe2dff9a
eney05 <eney05@ukr.net>
2016-07-11 22:17:44 +0300

    Процедурный подход функция DRAW

---
+++ snake2/snake2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym = '*';
+            Drav(x1, y1, sym);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Drav(x2, y2, sym2);
+
+            Console.ReadLine();
+
+        }
+
+        static void Drav (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
